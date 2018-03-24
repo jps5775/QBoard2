6803f3c13c9061f05de9a35c1c8d898faca18fda
laurenritter <ljritter1@gmail.com>
2018-03-24 12:31:31 -0400

    Added the stop scan section

---
+++ app/src/main/java/net/anew/joesema/qboard/QBoardAPI/LeDeviceListAdapter.java
@@ -0,0 +1,8 @@
+package net.anew.joesema.qboard.QBoardAPI;
+
+/**
+ * Created by laurenritter on 3/24/18.
+ */
+
+public class LeDeviceListAdapter {
+}
--- app/src/main/java/net/anew/joesema/qboard/QBoardAPI/ScanForDevice.java
+++ app/src/main/java/net/anew/joesema/qboard/QBoardAPI/ScanForDevice.java
@@ -1,7 +1,11 @@
 package net.anew.joesema.qboard.QBoardAPI;
 
+import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.Handler;
+
+import java.util.logging.Level;
 
 /**
  * Information found on Android Developer Website
@@ -12,10 +16,13 @@ public class ScanForDevice extends ListActivity {
     private BluetoothAdapter bluetoothAdapter;
     private boolean scanning;
     private Handler handler;
+    private LeDeviceListAdapter leDeviceListAdapter;
 
     //time alloted to scan for devices
     private static final long SCAN_PERIOD = 10000;
 
+
+    //start the scan
     private void scan(final boolean enable){
         if(enable) {
             handler.postDelayed(new Runnable() {
@@ -31,6 +38,20 @@ public class ScanForDevice extends ListActivity {
         }else{
             scanning = false;
             bluetoothAdapter.stopLeScan(leScanCallback);
+        }
+    }
+
+    //stop the scan
+    private BluetoothAdapter.LeScanCallback leScanCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+            runOnUiThread(new Runnable(){
+                @Override
+                public void run(){
+                    leDeviceListAdapter.addDevice(device);
+                    leDeviceListAdapter.notifyDataSetChanged();
+                }
+            });
         }
     }
 
