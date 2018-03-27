590313fd4dfe8bc4022bc4b304ab4fa04ff00708
laurenritter <ljritter1@gmail.com>
2018-03-27 10:23:05 -0400

    Remmade ScanForDevice

---
+++ app/src/main/java/net/anew/joesema/qboard/QBoardAPI/ScanForDevice.java
@@ -0,0 +1,53 @@
+package net.anew.joesema.qboard.QBoardAPI;
+
+import android.app.ListActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+
+import java.util.logging.Handler;
+
+/**
+ * Information taken from Android Developer Website
+ * Created by laurenritter on 3/27/18.
+ */
+
+public class ScanForDevice extends ListActivity {
+    private BluetoothAdapter bluetoothAdapter;
+    private boolean scanning;
+    private Handler handler;
+
+    private static final long SCAN_PERIOD = 10000;
+    private LeDeviceListApater leDeviceListAdpater;
+
+    private void scanLeDevice(final boolean enable){
+        if(enable){
+            //stops scanning after certain amount of time
+            handler.postDelayed(new Runnable(){
+                @Override
+                public void run(){
+                    scanning = false;
+                    bluetoothAdapter.stopLeScan(leScanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            scanning = true;
+            bluetoothAdapter.startLeScan(leScanCallback);
+        }else{
+            scanning = false;
+            bluetoothAdapter.stopLeScan(leScanCallback);
+        }
+    }
+
+    private BluetoothAdapter.LeScanCallback leScanCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+            runOnUiThread(new Runnable(){
+                @Override
+                public void run(){
+                    leDeviceListAdpater.addDevice(device);
+                    leDeviceListAdpater.notifyDataSetChanged();
+                }
+            });
+        }
+    }
+}
