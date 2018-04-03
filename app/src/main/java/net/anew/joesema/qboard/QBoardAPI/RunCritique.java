873031285efc83a69232f576c783d16cd9d9386d
laurenritter <ljritter1@gmail.com>
2018-04-03 09:26:14 -0400

    Critique stuff

---
+++ app/src/main/java/net/anew/joesema/qboard/QBoardAPI/RunCritique.java
@@ -0,0 +1,15 @@
+package net.anew.joesema.qboard.QBoardAPI;
+
+/**
+ * Created by laurenritter on 4/3/18.
+ */
+
+public class RunCritique {
+    private SimuBoard simuBoard;
+
+    public void runSpeed(){
+        if(simuBoard.getAccelerometer() >= 25 || simuBoard.getAccelerometer() <= 35){
+
+        }
+    }
+}
