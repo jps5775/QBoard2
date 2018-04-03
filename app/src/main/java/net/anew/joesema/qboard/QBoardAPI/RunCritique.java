c2dd8982f7df7c4f75600d513b99c72a67ea229d
Nick <nweld568@gmail.com>
2018-04-03 17:49:53 -0400

    Update some strings

--- app/src/main/java/net/anew/joesema/qboard/QBoardAPI/RunCritique.java
+++ app/src/main/java/net/anew/joesema/qboard/QBoardAPI/RunCritique.java
@@ -13,11 +13,18 @@ public class RunCritique {
     public String runSpeed(){ // We need to get the average data of the most previous run so we can get the data.
         speedEval = "";
         if((simuBoard.getAccelerometer()[1] >= 25) && (simuBoard.getAccelerometer()[1] <= 35)){ // Between 25 and 35 is average top speed
-            speedEval = "you fly";
+            speedEval = "You are achieving the average top snowboarding speed." +
+                        "Keep shredding!";
         } else if ((simuBoard.getAccelerometer()[1] > 35)) {
-            speedEval = "you super fly";
-        } else if(simuBoard.getAccelerometer()[1] < 25){
-            speedEval="you're slow and need to change your life";
+            speedEval = "You are going extremely fast at this point. Ensure you are wearing proper protective gear." +
+                        "At this speed, small changes in movement can result in injury.";
+        } else if(simuBoard.getAccelerometer()[1] < 25){ // advice for speed found on SnowboardLab
+            speedEval="Make sure that you:" +
+                    "1. Have both feet fully on the board. If any portion of your feet are sticking out then that will create lag." +
+                    "2. Your board may need more wax on it." +
+                    "3. Make sure that you are relaxing your knees. The lower you crouch down, the faster you will go." +
+                    "4. Riding flat on the snowboard will pick up more traction. You should usually not be touching heelside or toeside. This technique is called riding the edges." +
+                    "5. Make sure you are distributing the weight between both of your feet properly.";
         }
 
         return speedEval;
@@ -25,8 +32,8 @@ public class RunCritique {
 
     public String runGyro(){
         gyroEval = "";
-        if((simuBoard.getGyroscope()[1] >= 90) && (simuBoard.getGyroscope()[1] <= 270)){
-            gyroEval="you're just normal...sucks";
+        if((simuBoard.getGyroscope()[1] >= 90) && (simuBoard.getGyroscope()[1] <= 270)){ //We are saying that the average is between these degrees
+            gyroEval="Your rotation is on point.";
         }else if(simuBoard.getGyroscope()[1] < 90){
             gyroEval="you probably fell on your face";
         }else if(simuBoard.getGyroscope()[1] > 270){
@@ -42,7 +49,7 @@ public class RunCritique {
             gForceEval = "guess what? you're normal and no one cares what you do";
         }else if(simuBoard.getGyroscope(device)[1] < 1){
             gForceEval = "why are you crawling down the hill?";
-        }else if(simuBoard.getGyroscope(device)[1] > 2.5){
+        }else if(simuBoard.getGyroscope(device)[1] > 2.5){ //Olympic level
             gForceEval = "OKAY showoff";
         }
 
