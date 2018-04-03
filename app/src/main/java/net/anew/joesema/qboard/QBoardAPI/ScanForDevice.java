677285d06e1bf3a997b75e46ffd3168e2b61756d
Wesley Mauk <WesleyM77@users.noreply.github.com>
2018-04-03 00:43:05 -0400

    connection stuff needs to go in ocreate in scanfordevice

--- app/src/main/java/net/anew/joesema/qboard/QBoardAPI/ScanForDevice.java
+++ app/src/main/java/net/anew/joesema/qboard/QBoardAPI/ScanForDevice.java
@@ -1,14 +1,11 @@
 package net.anew.joesema.qboard.QBoardAPI;
 
 import android.Manifest;
-import android.app.ListActivity;
-import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattServer;
-import android.bluetooth.BluetoothGattServerCallback;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
@@ -20,15 +17,19 @@ import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanSettings;
 import android.content.Intent;
-
 import android.content.pm.PackageManager;
-import android.os.*;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.ParcelUuid;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import net.anew.joesema.qboard.R;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Information taken from https://www.bignerdranch.com/blog/bluetooth-low-energy-part-1/
@@ -57,8 +58,13 @@ public class ScanForDevice extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.pairphone_snowboard);
+
         bluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
+
+        // do the connect list stuff here
+
     }
 
     @Override
