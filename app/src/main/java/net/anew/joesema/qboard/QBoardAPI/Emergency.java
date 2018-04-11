1c3030fb3a75a4c8841e49a1a037d08e9d9d1832
Wesley Mauk <WesleyM77@users.noreply.github.com>
2018-04-11 15:38:10 -0400

    Made send emergency work It might not store the number correctly.

--- app/src/main/java/net/anew/joesema/qboard/QBoardAPI/Emergency.java
+++ app/src/main/java/net/anew/joesema/qboard/QBoardAPI/Emergency.java
@@ -3,12 +3,15 @@ package net.anew.joesema.qboard.QBoardAPI;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import net.anew.joesema.qboard.R;
 
 public class Emergency extends AppCompatActivity {
+
+    protected String currentNumber;
 
     private EditText emergencyNumber;
     private Button bSaveNumber;
@@ -21,7 +24,46 @@ public class Emergency extends AppCompatActivity {
         setContentView(R.layout.main_emergency_signal);
 
         // import the buttons and text fields
+        emergencyNumber = findViewById(R.id.editText2);
+        bSaveNumber = findViewById(R.id.button4);
+        bUseLastNumber = findViewById(R.id.useLastNumber);
+        bSendSignal = findViewById(R.id.button5);
+
+        // Joe's intent thing
         Intent intent = new Intent(Emergency.this, MainActivity.class);
+        intent.putExtra("eNumber", "8145550911");
+
+        //button stuff
+        bUseLastNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Is this right, joe? -Wesley's shitty guess
+                emergencyNumber.setText(savedInstanceState.getString("eNumber"));
+                saveNumber();
+            }
+        });
+
+        bSaveNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveNumber();
+            }
+        });
+
+        bSendSignal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Emergency.this, skipatrolcontacted.class);
+                intent.putExtra("eNumber", "8145550911");
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void saveNumber()
+    {
+        currentNumber = emergencyNumber.getText().toString();
+
     }
 
 
---
+++ app/src/main/java/net/anew/joesema/qboard/QBoardAPI/skipatrolcontacted.java
@@ -0,0 +1,51 @@
+package net.anew.joesema.qboard.QBoardAPI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import net.anew.joesema.qboard.R;
+
+import static java.lang.Thread.sleep;
+
+public class skipatrolcontacted extends AppCompatActivity {
+
+    private Button backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.skipatrolcontacted);
+
+        backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelEmergency();
+            }
+        });
+
+        try {
+            sleep(10000);
+            Intent intent = new Intent(skipatrolcontacted.this, AlertSent.class);
+            // hey joe, same thing here buddy
+            intent.putExtra("eNumber", savedInstanceState.getString("eNumber"));
+            startActivity(intent);
+        }
+        catch (Exception e)
+        {
+            cancelEmergency();
+        }
+
+    }
+
+
+    private void cancelEmergency()
+    {
+        Intent intent = new Intent(skipatrolcontacted.this, Emergency.class);
+        startActivity(intent);
+    }
+}
